2cb95df24d3d6a813ab5765bcac68749f62ef134
Mikhail Pestov <105435646+mnpestov@users.noreply.github.com>
2023-06-15 15:42:35 +0300

    Revert "бэкенд часть диплома"

--- errors/conflict-errors.js
+++
@@ -1,9 +0,0 @@
-const httpConstants = require('http2').constants;
-
-class ConflictError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = httpConstants.HTTP_STATUS_CONFLICT;
-  }
-}
-module.exports = ConflictError;
